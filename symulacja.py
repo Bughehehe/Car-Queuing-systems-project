3b5f66623d18b2373f280a369489723adb356703
aleksanderkolodziejczyk-a4bee <aleksander.kolodziejczyk@a4bee.com>
2023-12-24 00:06:39 +0100

    v2

--- symulacja.py
+++ symulacja.py
@@ -1,7 +1,7 @@
-import random
 import numpy as np
+import random
 
-SIMULATION_TIME = 1000000
+SIMULATION_TIME = 100000
 STATIC_TANKING_TIME = 60
 
 class car:
@@ -30,7 +30,7 @@ def lcg(x, a, b, m):
         yield x
 
 def random_uniform_sample(n, interval, seed=0):
-    #GLIBC
+
     a, b, m = 1103515245, 12345, 2 ** 32 -1
     bsdrand = lcg(seed, a, b, m)
 
@@ -39,7 +39,7 @@ def random_uniform_sample(n, interval, seed=0):
 
     for i in range(n):
         observation = (upper - lower) * (next(bsdrand) / (m)) + lower
-        sample.append(round(observation))
+        sample.append(int(observation))
 
     return sample
 
@@ -50,10 +50,9 @@ def random_exponential_sample(n, lambd, seed=0):
     sample = []
 
     for i in range(n):
-        # Generuj wartości z rozkładu eksponenty
         u = next(bsdrand) / (m)
         observation = -np.log(1 - u) / lambd
-        sample.append(observation)
+        sample.append(int(observation))
 
     return sample
 
@@ -64,15 +63,14 @@ def random_normal_sample(n, mean, std_dev, seed=0):
     sample = []
 
     for i in range(n):
-        # Generuj dwie wartości z rozkładu jednostajnego
         u1 = next(bsdrand) / (m)
         u2 = next(bsdrand) / (m)
 
-        # Transformacja Boxa-Mullera
+        # Box-Muller
         z0 = np.sqrt(-2 * np.log(u1)) * np.cos(2 * np.pi * u2)
         observation = mean + std_dev * z0
 
-        sample.append(observation)
+        sample.append(int(observation))
 
     return sample
 
@@ -88,32 +86,20 @@ can_next_car = True
 next_car_time = 0
 car_times = random_exponential(120, int(1000000/2))
 
-# print(len(cars_done))
-
-#zmienna losowa z rozkładu wykładniczego -> nowe auto -> szansa na przyjechanie auta (1/119)^k-1 * 1/120
-#Metoda odwrotnej dystrybuanty
-#Zrobić własny generator
-#pierwszy -> rozkład
-#drugi -> pierwszy + rozklad
-
-# tab_temp = 0
-# for n in range(1000000):
-#     tab_temp += int(random_exponential(120, 1))
-
-# print(tab_temp/1000000)
-
-
 while(second != SIMULATION_TIME):
 
     if(second == next_car_time):
         # Static time
-        distributor_A_buffer.append(car(car_id, second,  STATIC_TANKING_TIME))
+        # distributor_A_buffer.append(car(car_id, second,  STATIC_TANKING_TIME))
         # Uniform time
         # distributor_A_buffer.append(car(car_id, second,  int(random_uniform(0, 120)[0])))
+        # distributor_A_buffer.append(car(car_id, second,  random_uniform_sample(1, (0,120), random.randrange(0, 2**31 - 1))[0]))
         # Exponential time
         # distributor_A_buffer.append(car(car_id, second,  int(random_exponential(60, 1)[0])))
+        # distributor_A_buffer.append(car(car_id, second,  random_exponential_sample(1, 1/60, random.randrange(0, 2**31 - 1))[0]))
         # Normal time
         # distributor_A_buffer.append(car(car_id, second,  int(random_normal(60, 20)[0])))
+        distributor_A_buffer.append(car(car_id, second,  random_normal_sample(1, 60, 20, random.randrange(0, 2**31 - 1))[0]))
         car_id += 1
         can_next_car = True
 
@@ -123,21 +109,9 @@ while(second != SIMULATION_TIME):
     else: 
         distributor_A[0].tanking_time += 1
 
-    # Static time
     if len(distributor_A) != 0:
-        if distributor_A[0].tanking_time == STATIC_TANKING_TIME:
+        if distributor_A[0].time_to_tank == distributor_A[0].tanking_time:
             cars_done.append(distributor_A.pop(0))
-
-    # Exponential distribution
-    # if len(distributor_A) != 0:
-    #     if distributor_A[0].time_to_tank == distributor_A[0].tanking_time:
-    #         cars_done.append(distributor_A.pop(0))
-
-    # normal distribution
-    # if len(distributor_A) != 0:
-    #     if random_normal(60, 1) <= 58:
-    #         cars_done.append(distributor_A.pop(0))
-
 
     for waiting_car in distributor_A_buffer:
         waiting_car.waiting_time += 1
@@ -164,12 +138,15 @@ for car_final in cars_done:
 
 print(float(average_waiting_time-1)/float(len(cars_done)))
 
-# rus = random_exponential_sample(10000, 1/60, 4)
-# rus = random_uniform_sample(1000, (0,120), 4)
-rus = random_normal_sample(1000, 60, 20, 1)
 
-average_estimator = 0
-for i in rus:
-    print(int(i))
-    average_estimator += int(i)
-print(average_estimator/10000)
+# Obliczanie średniej estymatora
+
+# rus = random_exponential_sample(100000, 1/60, random.randrange(0, 2**31 - 1))
+# rus = random_uniform_sample(100000, (0,120), random.randrange(0, 2**31 - 1))
+# rus = random_normal_sample(100000, 60, 20, random.randrange(0, 2**31 - 1))
+
+# average_estimator = 0
+# for i in rus:
+#     print(int(i))
+#     average_estimator += int(i)
+# print(average_estimator/100000)
